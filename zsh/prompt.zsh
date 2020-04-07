4cd92046424fb05dc3603e5ed3ab7d4796413635
wawulong <wawulong@gmail.com>
2020-04-07 23:30:59 +0800

    Change prompt string

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -51,7 +51,7 @@ need_push () {
 }
 
 directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+  echo "%{$fg_bold[cyan]%}%2/%\/%{$reset_color%}"
 }
 
 battery_status() {
@@ -67,6 +67,7 @@ battery_status() {
 }
 
 export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n$(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
